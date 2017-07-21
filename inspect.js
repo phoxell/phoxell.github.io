8255b1f77ea4ea89b808ae60d4cf7b1b817aba67
sologram <sologram@gmail.com>
2017-07-22 01:00:33 +0800

    init

--- inspect.js
+++ inspect.js
@@ -4,14 +4,13 @@ d2=document.getElementById("d2");
 d3=document.getElementById("d3");
 
 function go(){
-    navigator.bluetooth.requestDevice({ filters: [{ services: ['00000000-0000-1000-8000-00805f9b34fb'] }] })
+    navigator.bluetooth.requestDevice({filters:[{services: ['00000000-0000-1000-8000-00805f9b34fb']}]})
     .then(device => device.gatt.connect())
     .then(server => server.getPrimaryService('00000000-0000-1000-8000-00805f9b34fb'))
     .then(service => service.getCharacteristic('00000002-0000-1000-8000-00805f9b34fb'))
     .then(characteristic => characteristic.startNotifications())
     .then(characteristic => {
-        characteristic.addEventListener('characteristicvaluechanged',
-        handleCharacteristicValueChanged);
+        characteristic.addEventListener('characteristicvaluechanged',handleCharacteristicValueChanged);
         console.log('Notifications have been started.');
     })
     .catch(error => { console.log(error); });
@@ -24,60 +23,3 @@ function handleCharacteristicValueChanged(event) {
     d2.innerHTML='[2] ';//+d[2];
     d3.innerHTML='[3] ';//+d[3];
 }
-
-function search(){
-	document.getElementById("demo").innerHTML="My First JavaScript";
-	let options = {
-	optionalServices:['00000000-0000-1000-8000-00805f9b34fb']
-	};
-	let filters = [];
-	let filterName = document.querySelector('#name').value;
-    if (filterName) {
-    filters.push({name: filterName});
-	 options.filters = filters;
-    }else{
-     options.acceptAllDevices = true;
-    }
-  
-	navigator.bluetooth.requestDevice(options).then(device => {
-	return	device.gatt.connect();
-	
-		       }).then(server => {		
-			 return server.getPrimaryService('00000000-0000-1000-8000-00805f9b34fb');
-			}).then(service => {
-			 chosenHeartRateService = service;
-			return Promise.all([
-			      service.getCharacteristic('00000002-0000-1000-8000-00805f9b34fb').then(Notifications),
-				  service.getCharacteristic('00000001-0000-1000-8000-00805f9b34fb').then(w),
-				  service.getCharacteristic('00000003-0000-1000-8000-00805f9b34fb').then(read)
-			]);
-	
-             }).catch(error => {
-			document.getElementById("demo").innerHTML=error;
-			});
-}
-
-function Notifications(characteristic){
-	 characteristic.startNotifications();
-}
-
-function read(characteristic){
-   
-	return characteristic.readValue().then(sensorLocationData => {
-		
-		let sensorLocation = sensorLocationData.getUint8(1);
-			document.getElementById("data").innerHTML="sensorLocation"+sensorLocation;
-		
-	});
-	
-}
-
-function w(characteristic){
-
-    let resetEnergyExpended = new Uint8Array.of(65,75);
-    controlPoint.writeValue(resetEnergyExpended);
-}
-
-
-
-
