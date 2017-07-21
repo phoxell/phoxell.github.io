2bec8badf4f893f73e6657b461fffcc674a11ab6
sologram <sologram@gmail.com>
2017-07-22 01:36:47 +0800

    init

--- inspect.js
+++ inspect.js
@@ -1,14 +1,27 @@
-var d0, d1, d2, d3;
+var d00, d01, d02, d03, d04, d05, d06, d07, d08, d09;
+var d10, d11, d12, d13, d14, d15, d16, d17, d18, d19;
 
 function go(){
-    d0=document.getElementById('c0');
-    d1=document.getElementById('c1');
-    d2=document.getElementById('c2');
-    d3=document.getElementById('c3');
-    d0.innerHTML='0]';
-    d1.innerHTML='1]';
-    d2.innerHTML='2]';
-    d3.innerHTML='3]';
+    d00=document.getElementById('c00');
+    d01=document.getElementById('c01');
+    d02=document.getElementById('c02');
+    d03=document.getElementById('c03');
+    d04=document.getElementById('c04');
+    d05=document.getElementById('c05');
+    d06=document.getElementById('c06');
+    d07=document.getElementById('c07');
+    d08=document.getElementById('c08');
+    d09=document.getElementById('c09');
+    d10=document.getElementById('c10');
+    d11=document.getElementById('c11');
+    d12=document.getElementById('c12');
+    d13=document.getElementById('c13');
+    d14=document.getElementById('c14');
+    d15=document.getElementById('c15');
+    d16=document.getElementById('c16');
+    d17=document.getElementById('c17');
+    d18=document.getElementById('c18');
+    d19=document.getElementById('c19');
     navigator.bluetooth.requestDevice({filters:[{services: ['00000000-0000-1000-8000-00805f9b34fb']}]})
     .then(device => device.gatt.connect())
     .then(server => server.getPrimaryService('00000000-0000-1000-8000-00805f9b34fb'))
@@ -27,4 +40,20 @@ function handleCharacteristicValueChanged(event) {
     d1.innerHTML='[1] '+d.getInt8(1);
     d2.innerHTML='[2] '+d.getInt8(2);
     d3.innerHTML='[3] '+d.getInt8(3);
+    d0.innerHTML='[4] '+d.getInt8(4);
+    d1.innerHTML='[5] '+d.getInt8(5);
+    d2.innerHTML='[6] '+d.getInt8(6);
+    d3.innerHTML='[7] '+d.getInt8(7);
+    d0.innerHTML='[8] '+d.getInt8(8);
+    d1.innerHTML='[9] '+d.getInt8(9);
+    d0.innerHTML='[10] '+d.getInt8(10);
+    d1.innerHTML='[11] '+d.getInt8(11);
+    d2.innerHTML='[12] '+d.getInt8(12);
+    d3.innerHTML='[13] '+d.getInt8(13);
+    d0.innerHTML='[14] '+d.getInt8(14);
+    d1.innerHTML='[15] '+d.getInt8(15);
+    d2.innerHTML='[16] '+d.getInt8(16);
+    d3.innerHTML='[17] '+d.getInt8(17);
+    d0.innerHTML='[18] '+d.getInt8(18);
+    d1.innerHTML='[19] '+d.getInt8(19);
 }
